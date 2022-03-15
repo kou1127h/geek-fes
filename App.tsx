aee5c1c1f0cc1d1adc3ac9ae244c0357651a7552
K.Hayashi <pg5027ace@gmail.com>
2022-03-15 21:06:11 +0900

    app調整

--- App.tsx
+++ App.tsx
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useCallback, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [roundCount, setRoundCount] = useState<number[]>([]);
+
+  const handlePressEndButton = useCallback(() => {
+    if (count > 0) {
+      setRoundCount([...roundCount, count]);
+      setCount(0);
+    }
+  }, [count, roundCount]);
+
+  const handlePressResetButton = useCallback(() => {
+    setRoundCount([]);
+    setCount(0);
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ flexDirection: "row" }}>
+        {
+          // ほんとはFlatListを使いたい
+          roundCount.map((val, index) => (
+            <Text key={index}>
+              {index + 1}: {val}{" "}
+            </Text>
+          ))
+        }
+      </View>
+      <Text>{count}</Text>
+      <Button title="+1" onPress={() => setCount(count + 1)} />
+      <Button title="end round" onPress={handlePressEndButton} />
+
+      <Button title="reset all" onPress={handlePressResetButton} />
+      <StatusBar style="auto" animated />
     </View>
   );
 }
@@ -13,8 +42,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
